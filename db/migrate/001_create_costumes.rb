e11a37afe5eefc8ab75111ce3e35da007b58b833
Patrick J Louis <lumenfuzz@gmail.com>
2018-04-21 21:15:29 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -11,7 +11,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |t|
       t.string :name
-      t.integer :release_date
+      t.double :price
       t.string :director
       t.string :lead
       t.boolean :in_theaters
