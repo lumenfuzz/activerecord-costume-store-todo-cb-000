18d1a31ba61acb57f2a2a05ddfa8f6b5153182b6
Patrick J Louis <lumenfuzz@gmail.com>
2018-04-21 21:02:54 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,12 @@
 
 ActiveRecord::Schema.define(version: 3) do
 
+  create_table "costumes", force: :cascade do |t|
+    t.string "title"
+    t.integer "release_date"
+    t.string "director"
+    t.string "lead"
+    t.boolean "in_theaters"
+  end
+
 end
