1ed4873ec3b88dd0b11569e5ba4b2b1fe941c3cf
Patrick J Louis <lumenfuzz@gmail.com>
2018-04-21 21:09:09 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,3 +1,1 @@
 # Create your costume_stores migration here
-class CreateCostumeStores < ActiveRecord::Migration[4.2]
-end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,3 +1,1 @@
 # Create your haunted_houses migration here
-class CreateHauntedHouses < ActiveRecord::Migration[4.2]
-end
